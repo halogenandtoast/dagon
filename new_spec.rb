fc6e1dfde7e2783e2f18168d20e446e5bd00a007
Caleb Thompson <cjaysson@gmail.com>
2013-01-10 23:00:44 -0500

    All a language really is is a class called New

--- new_spec.rb
+++ new_spec.rb
@@ -2,52 +2,65 @@ require 'rspec'
 require 'active_support/core_ext/string/strip'
 require 'pry'
 
-def parse(code)
-  @results = {}
-  lines = code.split("\n")
-  lines.each do |line|
-    parts = line.split(': ')
-    string_regexp = /(?<quote>"|')(?<string>.*)\k<quote>/
-    if r = string_regexp.match(parts[1])
-      result = r['string']
-    else
-      result = Integer(parts[1])
-    end
+class New
+  attr_accessor :results
 
-    @results[parts[0]] = result
+  def initialize(code)
+    self.code = code
   end
+
+  def parse
+    self.results = {}
+    lines = code.split("\n")
+    lines.each do |line|
+      parts = line.split(': ')
+      string_regexp = /(?<quote>"|')(?<string>.*)\k<quote>/
+      if r = string_regexp.match(parts[1])
+        result = r['string']
+      else
+        result = Integer(parts[1])
+      end
+
+      self.results[parts[0]] = result
+    end
+    self
+  end
+
+  private
+
+  attr_accessor :code
 end
 
-describe "it" do
+describe New do
   it 'can assign a variable' do
-    parse(%{x: 1})
+    new = New.new(%{x: 1}).parse
 
-    expect(@results["x"]).to eq(1)
+    expect(new.results["x"]).to eq(1)
   end
 
   it 'can assign another variable' do
-    parse(<<-CODE.strip_heredoc)
+    new = New.new(<<-CODE.strip_heredoc).parse
       x: 1
       y: 2
     CODE
 
-    expect(@results["x"]).to eq(1)
-    expect(@results["y"]).to eq(2)
+    expect(new.results["x"]).to eq(1)
+    expect(new.results["y"]).to eq(2)
   end
 
   it 'can assign a string even' do
-    parse(%{x: "stringy"})
+    new = New.new(%{x: "stringy"}).parse
 
-    expect(@results["x"]).to eq("stringy")
+    expect(new.results["x"]).to eq("stringy")
   end
 
   it "can has '' string syntax too" do
-    parse(%{x: 'stringy'})
+    new = New.new(%{x: 'stringy'}).parse
 
-    expect(@results["x"]).to eq("stringy")
+    expect(new.results["x"]).to eq("stringy")
   end
 
   it "doesn't work with mismatched quote types" do
-    expect { parse(%{x: "stringy'}) }.to raise_error(ArgumentError)
+    expect { New.new(%{x: "stringy'}).parse }.to raise_error(ArgumentError)
   end
 end
