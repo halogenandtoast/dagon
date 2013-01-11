d50c4436fa14ca9a9c6557701456d764c487945f
Caleb Thompson <cjaysson@gmail.com>
2013-01-10 21:50:46 -0500

    assigns integers to variables.

---
+++ new_spec.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+require 'active_support/core_ext/string/strip'
+require 'pry'
+
+def parse(code)
+  lines = code.split("\n")
+  lines.each do |line|
+    parts = line.split(': ')
+    instance_variable_set(:"@#{parts[0]}", parts[1].to_i)
+  end
+end
+
+describe "it" do
+  it 'can assign a variable' do
+    parse(<<-CODE.strip_heredoc)
+      x: 1
+    CODE
+
+    expect(@x).to eq(1)
+  end
+
+  it 'can assign another variable' do
+    parse(<<-CODE.strip_heredoc)
+      y: 2
+    CODE
+
+    expect(@y).to eq(2)
+  end
+end
