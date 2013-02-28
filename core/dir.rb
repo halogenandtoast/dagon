6de90c00f1b998c1bc10ed89465c22d4c9b8f214
halogenandtoast <halogenandtoast@gmail.com>
2013-02-27 23:25:46 -0500

    Add Dir.glob

--- core/dir.rb
+++ core/dir.rb
@@ -9,6 +9,10 @@ module Dagon
         add_class_method "getwd", ->(vm, ref) {
           vm.get_class("String").dagon_new(vm, Dir.getwd)
         }
+        add_class_method "glob", ->(vm, ref, glob_path) {
+          paths = Dir.glob(glob_path.value).map { |path| vm.get_class("String").dagon_new(vm, path) }
+          vm.get_class("Array").dagon_new(vm, paths)
+        }
       end
     end
   end
