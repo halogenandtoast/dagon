8e29b64edf264c4f9987069b1f0fef131c2bafef
Matthew Mongeau <halogenandtoast@gmail.com>
2013-03-03 00:07:06 -0500

    Remove unused File definition.

--- core/file.rb
+++ core/file.rb
@@ -1,8 +1,5 @@
 module Dagon
   module Core
-    class DG_File < DG_IO
-    end
-
     class DG_FileClass < DG_IOClass
       def initialize vm
         super("File", vm.get_class("IO"))
@@ -16,11 +13,6 @@ module Dagon
         add_class_method "join", ->(vm, ref, *paths) {
           vm.get_class("String").dagon_new(vm, File.join(paths.map(&:value)))
         }
-      end
-
-      def dagon_new interpreter, name, mode
-        descriptor = File.open(name.value, mode.value)
-        DG_File.new(descriptor, self)
       end
     end
   end
