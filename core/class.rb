405072488d3a5b60947580308db7af8e60206912
halogenandtoast <halogenandtoast@gmail.com>
2014-07-18 16:31:16 -0400

    Fix idg to handle ^C and ^D
    * Add trap to class
    * idg now handles ^D by exiting
    * idg now traps ^C and exits
    * Add trap example

--- core/class.rb
+++ core/class.rb
@@ -13,6 +13,7 @@ module Dagon
           print: ->(vm, ref, *args) { print *args.map(&:to_s) },
           gets: ->(vm, ref, *args) { vm.string($stdin.gets) },
           system: ->(vm, ref, *args) { vm.string(Kernel.send(:`, *args.map(&:to_s))) },
+          trap: ->(vm, ref, *args) { trap(args[0].to_s) { args[1].call(vm) } },
           eval: ->(vm, ref, *args) {
             tokens = Dagon::Scanner.tokenize(args[0].value, '(eval)') do |error|
               vm.error("SyntaxError", error)
